ee5df854892ecb877f19c42c866e99c25a263513
NelsonZeng25 <nelson.zeng25@gmail.com>
2019-10-31 15:37:34 -0400

    Finished lab 3 (No comments)

--- G80_Lab3/main.c
+++ G80_Lab3/main.c
@@ -10,17 +10,13 @@
 #include "./drivers/inc/int_setup.h"
 int main() {
 	
-	/*
-	while(1) {
-		write_LEDs_ASM(read_slider_switches_ASM());
-	}
 	
-
+/*
 	//HEX_clear_ASM(HEX1 | HEX3 | HEX4 | HEX5);
 	//HEX_flood_ASM(HEX0 | HEX2 | HEX3 | HEX4 | HEX5);
 	//HEX_write_ASM(HEX1 | HEX4, 15);
 	//HEX_flood_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5);
-	HEX_write_ASM(HEX1, 15);
+	//HEX_write_ASM(HEX1, 15);
 	while(1){
 		write_LEDs_ASM(read_slider_switches_ASM());
 		if(read_slider_switches_ASM() & 0x200){
@@ -81,25 +77,25 @@ int main() {
 	hps_tim.enable = 1;
 	HPS_TIM_config_ASM(&hps_tim); //Config timer 0
 	
-	//Pushbutton polling
+	//This timer is for the pushbutton polling
 	HPS_TIM_config_t hps_tim_pb;
 	hps_tim_pb.tim = TIM1;
 	hps_tim_pb.timeout = 5000;
 	hps_tim_pb.LD_en = 1;
 	hps_tim_pb.INT_en = 1;
 	hps_tim_pb.enable = 1;
-	HPS_TIM_config_ASM(&hps_tim_pb); //config timer 1
-	
+	HPS_TIM_config_ASM(&hps_tim_pb); //config timer 2
+
 	//Declare time unit variables
-	int micro = 0, second = 0, minute = 0, timerstart = 0;
+	int milli = 0, second = 0, minute = 0, timerstart = 0;
 	while (1) {
 		// Checks if timer was started
 		if (HPS_TIM_read_INT_ASM(TIM0) && timerstart) {
 			HPS_TIM_clear_INT_ASM(TIM0);
-			micro += 10; 				//Timer is for 10 milliseconds
+			milli += 10; 				//Timer is for 10 milliseconds
 			//When microseconds reach 1000 (1s), increment seconds and reset mircoseconds
-			if (micro >= 1000) {
-				micro = 0;
+			if (milli >= 1000) {
+				milli = 0;
 				second++;
 				// Reset seconds when > 60 then increment minutes
 				if (second >= 60) {
@@ -112,8 +108,8 @@ int main() {
 				}
 			}
 			//Display every value and convert the count to ascii values
-			HEX_write_ASM(HEX0, (micro % 100) / 10);	// Micro increments in 10 microseconds so we must modulo and divide by 10 to get the digit at 0.01 s
-			HEX_write_ASM(HEX1, micro / 100);			// Divide Micro by 100 to get 0.1s digit
+			HEX_write_ASM(HEX0, (milli % 100) / 10);	// Micro increments in 10 microseconds so we must modulo and divide by 10 to get the digit at 0.01 s
+			HEX_write_ASM(HEX1, milli / 100);			// Divide Micro by 100 to get 0.1s digit
 			HEX_write_ASM(HEX2, second % 10);			// Modulo seconds to get 1s digit
 			HEX_write_ASM(HEX3, second / 10);			// Divide by 10 to get 10s digit
 			HEX_write_ASM(HEX4, minute % 10);			// Same for minutes
@@ -121,7 +117,6 @@ int main() {
 		}
 		//Pushbuttons polling
 		if (HPS_TIM_read_INT_ASM(TIM1)) {
-			HPS_TIM_clear_INT_ASM(TIM1);
 			int pushbutton = 0xF & read_PB_data_ASM();
 			
 			//Start
@@ -134,17 +129,12 @@ int main() {
 			}
 			//Reset
 			else if (pushbutton & 2) {
-				micro = 0;
+				milli = 0;
 				second = 0;
 				minute = 0;
 				timerstart = 0;
 
-				HEX_write_ASM(HEX0, 0);
-				HEX_write_ASM(HEX1, 0);
-				HEX_write_ASM(HEX2, 0);
-				HEX_write_ASM(HEX3, 0);
-				HEX_write_ASM(HEX4, 0);
-				HEX_write_ASM(HEX5, 0);
+				HEX_write_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5, 0);
 			}
 		}
 	}
@@ -193,8 +183,8 @@ int main() {
 		}
 		//if pushbutton flag active, the ISR is active, we do something according to which button pressed
 		if (pb_int_flag != 0){
-			if(pb_int_flag == 8) timerstart = 1;
-			else if(pb_int_flag == 4) timerstart = 0;
+			if(pb_int_flag == 8 && !timerstart) timerstart = 1;
+			else if(pb_int_flag == 4 && timerstart) timerstart = 0;
 			else if(pb_int_flag == 2) {
 				micro = 0;
 				second = 0;
@@ -210,5 +200,6 @@ int main() {
 			pb_int_flag = 0;
 		}
 	}
+
 	return 0;
 }
