0fc7df1c1a26848b9f7ea91af997046799a22e32
NelsonZeng25 <nelson.zeng25@gmail.com>
2019-11-21 15:33:03 -0500

    Finished part 1 make wave function

--- G80_Lab5/main.c
+++ G80_Lab5/main.c
@@ -10,6 +10,25 @@
 #include "./drivers/inc/HEX_displays.h"
 #include "./drivers/inc/slider_switches.h"
 
+#include <math.h>
+
+// Make Waves Part 1
+float make_wave(float f, int t) {
+	float index = (f * t) % 48000;
+	int x = ceil(index);			// x holds upper int of index
+	int y = floor(index);			// y holds lower int of index
+	float signal;
+	if (x!=y) {						// Check if index isn't a whole number
+		int remainder = index - y;	// Get remainder
+		signal = (1-remainder)*sine[y] + remainder * sine[x];	// Linear interpolation of wavetable
+	} else {
+		signal = sine[x];			// If index is a whole number, simply get the index of the wavetable
+	}
+
+	return signal;
+}
+
+
 int main() {
 	// Audio Part 0
 	int hi = 0x00FFFFFF; 	// High signal
@@ -17,13 +36,15 @@ int main() {
 	int x = hi;
 	int i;
 	while(1){
-		for(i = 0 ; i < 240;){ 			// To get a frequency of 100Hz, there must be 480 samples per full cycle
-			if(audio_write_ASM(x))		// Therefore, there must be 240 samples per half cycle
+		for(i = 0 ; i < 240;){ 			// To get a frequency of 100Hz, there must be 480 samples per full cycle (48000 Samples/s / 100 Hz)
+			if(audio_write_ASM(x))		// Therefore, there must be 240 samples per half cycle (480 samples/cycle / 2)
 				i++;
 		}
 		x =(x==hi)?lo:hi;				// Invert the x setting (high -> low, low -> high)
 	}
 
 
+	// Control Waves Part 2
+	
 	return 0;
 }
